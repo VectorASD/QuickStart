6e8685e521743aa125b4d79366686eb996420f28
VectorASD <60642904+VectorASD@users.noreply.github.com>
2025-12-19 00:51:51 +0700

    Update QuickBlender.py

--- QuickBlender.py
+++ QuickBlender.py
@@ -55,24 +55,56 @@ def exit(*arr, sep=" ", end="\n", color="red"):
     print(*arr, sep=sep, end=end, color=color)
     raise MySystemExit
 
+def security_wrap(func):
+    def wrapper(*a, **kw):
+        try: return func(*a, **kw)
+        except MySystemExit: raise
+        except:
+            for line in traceback.format_exc().splitlines():
+                print(line, color="red")
+            raise MySystemExit
+    return wrapper
+
 def pprint(*a, **kv):
     print(pformat(*a, **kv))
+
+def redraw_chain(range, cb, end=None):
+    it = iter(range)
+    @security_wrap
+    def step():
+        try: number = next(it)
+        except StopIteration:
+            if end is not None: end()
+            return
+
+        cb(number)
+
+        for area in bpy.context.screen.areas:
+            if area.type == 'VIEW_3D':
+                area.tag_redraw()
+
+        return bpy.app.timers.register(step)
+    bpy.app.timers.register(step)
+
+scene = bpy.context.scene
+
+def frame_chain(range, cb):
+    def step(frame):
+        scene.frame_set(frame)
+        cb(frame)
+    def end():
+        scene.frame_set(current)
+    current = scene.frame_current
+    redraw_chain(range, step, end)
 
 # ~~~ codegup ~~~
 
 
 
+@security_wrap
 def solve():
     print("MEOW!")
     # exit("Упс", "ошибочка!", sep=", ")
     raise ValueError("Слишком громкое мяукание!")
 
-
-
-# ~~~ codegup ~~~
-
-try: solve()
-except MySystemExit: pass
-except:
-    for line in traceback.format_exc().splitlines():
-        print(line, color="red")
+solve()
